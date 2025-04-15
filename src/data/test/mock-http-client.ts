390c81ca9dd7391ce17e354e09335e8afc049ee6
Vinicius Soares <vinicius.soares852@gmail.com>
2025-04-14 22:46:38 -0300

    refactor: group imports in index files

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -1,5 +1,4 @@
-import { type HttpPostParams, type HttpPostClient } from '@/data/protocols/http/http-post-client'
-import { type HttpResponse, HttpStatusCode } from '@/data/protocols/http/http-response'
+import { type HttpResponse, HttpStatusCode, type HttpPostParams, type HttpPostClient } from '@/data/protocols/http'
 
 export class HttpPostClientSpy<T, R> implements HttpPostClient<T, R> {
   url?: string
