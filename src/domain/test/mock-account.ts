e0f4f6f04342f1ed713f09b2b1315af7c8fe594e
Vinicius Soares <vinicius.soares852@gmail.com>
2025-04-14 22:34:32 -0300

    test: ensure RemoteAuthentication returns an Account on success

--- src/domain/test/mock-account.ts
+++ src/domain/test/mock-account.ts
@@ -1,7 +1,12 @@
+import { type AccountModel } from '../models/account-model'
 import { type AuthenticationParams } from '../usecases/authentication'
 import { faker } from '@faker-js/faker'
 
 export const mockAuthentication = (): AuthenticationParams => ({
   email: faker.internet.email(),
   password: faker.internet.password()
+})
+
+export const mockAccountModel = (): AccountModel => ({
+  accessToken: faker.string.uuid()
 })
